adf03c9b039e32b2cf63e28e332f9ced6951cbd6
KP_349 <KP_349@FANARIER>
2022-10-15 18:43:02 +0200

    Добавьте файлы проекта.

---
+++ Logical expressions/Program.cs
@@ -0,0 +1,26 @@
+﻿//2.1.Логические выражения. Плотников Кирилл Андреевич 22-ИСП-2/2
+//№варианта: 16, Записать условие, которое является истинным, когда только одно из чисел X,Y и Z кратно пяти.
+
+try
+{
+
+Console.ForegroundColor = ConsoleColor.Green;
+   
+Console.WriteLine("Введите X:");
+Double X = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите Y:");
+Double Y = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите Z:");
+Double Z = double.Parse(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+long a = (long)(X * Y * Z);
+if ((a % 5 == 0) && (a % 25 != 0)) Console.WriteLine("только одно из чисел X,Y и Z кратно пяти");
+else Console.WriteLine("не подходит под условия");
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+}
+catch (Exception ex)
+{ Console.WriteLine(ex.ToString()); }
